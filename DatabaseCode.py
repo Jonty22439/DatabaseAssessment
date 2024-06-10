576217804bb2554f6e9ec7fb9393726b64be0e6d
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-06-10 15:18:55 +1200

    New Function
    I added a new function for the Nationalities table. Hopefully I will learn and setup joins in the functions to make it all look a lot nicer. I experimented with adding a way of writing your own queries as the user, but I haven't got it working to a satisfactory level so I just removed it for now.

--- DatabaseCode.py
+++ DatabaseCode.py
@@ -5,10 +5,12 @@ import sqlite3
 # variables
 DATABASE = "Languages.db"
 
-# Prints all languages in order of how many people speak it
+# funtions
+# prints all languages in order of how many people speak it
 def printlang():
     db = sqlite3.connect(DATABASE)
     cursor = db.cursor()
+    # SQL quere
     SQL = "SELECT * FROM Languages ORDER BY million_speakers DESC;"
     cursor.execute(SQL)
     results = cursor.fetchall()
@@ -18,10 +20,11 @@ def printlang():
         print(f"{result[1] :<23}{result[2]:<14}{result[3]:<28}{result[4]:<11}")
     db.close()
 
-# Prints all countries in order of population
+# prints all countries in order of population
 def printcountry():
     db = sqlite3.connect(DATABASE)
     cursor = db.cursor()
+    # SQL quere
     SQL = "SELECT * FROM Countries ORDER BY population_million DESC;"
     cursor.execute(SQL)
     results = cursor.fetchall()
@@ -31,18 +34,34 @@ def printcountry():
         print(f"{result[1] :<23}{result[2]:<14}{result[3]:<28}")
     db.close()
 
-# The main loop that recieves input and runs one of the programs depending on input
+# prints all nationalities in the database
+def printnationality():
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
+    # SQL quere
+    SQL = "SELECT * FROM Nationalities;"
+    cursor.execute(SQL)
+    results = cursor.fetchall()
+    # prints everything neatly 
+    print("NATIONALITY            COUNTRY")
+    for result in results:
+        print(f"{result[1] :<23}{result[2]:<14}")
+    db.close()
+
+# the main loop that recieves input and runs one of the programs depending on the input given by the user
 while True:
     print("                 __              __   ___  ___     __        _____      ___      ___  ___   ")
     print("|      /| |\  | /    |   |   /| /    |___  \_     |  \    /|   |    /| |__/   /| \_  |___   ")
     print("|     /_| | \ | | __ |   |  /_| | __ |       \    |   |  /_|   |   /_| |  \  /_|   \ |      ")
     print("|___ /  | |  \| \__| \__/  /  | \__| |___  __/    |__/  /  |   |  /  | |__/ /  | __/ |___   ")
-    user_input = input("What would you like to do?\n1. print all languages in order of people who speak it\n2. print all countries in the database \n3. close the program\n")
+    user_input = input("What would you like to do?\n1. print all languages in order of people who speak it\n2. print all countries in the database\n3. print all nationalities\n5. close the program\n")
     if user_input == "1":
         printlang()
     elif user_input == "2":
         printcountry()
     elif user_input == "3":
+        printnationality()
+    elif user_input == "4":
         break
     else:
         print("\nPlease input a valid number")
